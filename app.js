46511815fede72172effcf815bf49e4874c86143
kiruphathanan <kirupha2005@gmail.com>
2025-03-19 16:31:06 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+var body=document.querySelector("body");
+
+function dark(){
+  body.className="";
+  body.classList.add("dark");
+}
+function bright(){
+   body.className="";
+   body.classList.add("bright");
+}
